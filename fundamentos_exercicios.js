fb24e83221bfc8afa85c780457c80e90eaed0b31
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2024-01-29 20:01:40 -0300

    exercícios

---
+++ fundamentos_exercicios.js
@@ -0,0 +1,12 @@
+// ex: 1
+console.log('Ola, Mundo!')
+
+// ex: 2
+let string = '1234'
+let number = Number(string)
+console.log(typeof number)
+
+// ex: 3
+let message = 'JavaScript é incrível'
+let numeroDeCaracteres = message.length
+let numeroDePalavras = message.split(" ").length
